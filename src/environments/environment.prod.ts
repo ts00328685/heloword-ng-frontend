1bdfea200d8d45c4df5c190b30b352be72ac8740
Ryan Tzeng <ryan.tzeng@compax.at>
2022-04-12 01:03:13 +0800

    update base href

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   baseHref: '/k8s/ng-frontend/v1',
   appVersion: '0.1',
-  backendBaseUrl: '/k8s/micro-infra-gateway/v1',
+  backendBaseUrl: '/k8s/micro-infra-gateway/v1/',
   googleClientId: '268421074885-cn4qtlas4hep25tt7f0gaak8qh557fbu.apps.googleusercontent.com',
   production: true,
   loggerSettings: {
