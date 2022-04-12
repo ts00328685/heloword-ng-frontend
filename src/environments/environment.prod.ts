16e2d464f3a9d3b38d91f6279416639befab5d10
Ryan Tzeng <ryan.tzeng@compax.at>
2022-04-12 23:29:13 +0800

    update nginx settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appVersion: '0.1',
-  backendBaseUrl: '/k8s/micro-infra-gateway/v1/',
+  backendBaseUrl: '/k8s/micro-infra-gateway/v1',
   googleClientId: '268421074885-cn4qtlas4hep25tt7f0gaak8qh557fbu.apps.googleusercontent.com',
   production: true,
   loggerSettings: {
